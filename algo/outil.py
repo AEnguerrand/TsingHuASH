f627ffe2cfae302c1667033ab9609c5da2c4d8bc
ValentinYiu <valentinY@protonmail.com>
2019-03-01 04:09:04 +0800

    trio

--- algo/outil.py
+++ algo/outil.py
@@ -3,6 +3,10 @@ def swap_special(L,i,j):
     swapped_L = L[:i] + [L[j]] + list(reversed(L[i+1:j])) + [L[i]] + L[j+1:]
     return swapped_L
 
+def swap_triolisme(L,i,j,k):
+    swapped_L = L[:i] + [L[k]] + list(reversed(L[j+1:k])) + [L[i]] + L[i+1:j] + [L[j]] + L[k+1:]
+    return swapped_L
+
 def trieur_orientation(Pictures):
     # Renvoie la liste des id avec d'abord les indices horizontaux puis les indices verticaux par paires arbitraires
     # Ex: [5,4,8,9,(1,2),(6,7)] 
